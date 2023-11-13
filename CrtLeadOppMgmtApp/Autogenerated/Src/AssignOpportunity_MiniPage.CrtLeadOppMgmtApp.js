ab46f550d764c2507bae1f55434b754f11eb7edf
Viratajay-18 <ajaymani413@psnacet.edu.in>
2023-11-13 17:39:16 +0530

    the very first commit

---
+++ CrtLeadOppMgmtApp/Autogenerated/Src/AssignOpportunity_MiniPage.CrtLeadOppMgmtApp.js
@@ -0,0 +1,154 @@
+﻿define("AssignOpportunity_MiniPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"name": "PageTitle",
+				"values": {
+					"visible": false
+				}
+			},
+			{
+				"operation": "insert",
+				"name": "Title",
+				"values": {
+					"type": "crt.Label",
+					"caption": "#MacrosTemplateString(#ResourceString(Title_caption)#)#",
+					"labelType": "headline-2",
+					"labelThickness": "default",
+					"labelEllipsis": false,
+					"labelColor": "#0D2E4E",
+					"labelBackgroundColor": "transparent",
+					"labelTextAlign": "start",
+					"visible": true
+				},
+				"parentName": "TitleContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Type",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 1,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "#ResourceString(Type_label)#",
+					"labelPosition": "above",
+					"control": "$LookupAttribute_uvmgqg9",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": [],
+					"visible": true,
+					"placeholder": "",
+					"tooltip": ""
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Group",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 2,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.LookupAttribute_5lqbnzx",
+					"labelPosition": "above",
+					"control": "$LookupAttribute_5lqbnzx",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": [],
+					"visible": true,
+					"placeholder": "",
+					"tooltip": ""
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "Owner",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 3,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.LookupAttribute_0gzvy5s",
+					"labelPosition": "above",
+					"control": "$LookupAttribute_0gzvy5s",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": []
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "addRecord_kvju3si",
+				"values": {
+					"code": "addRecord",
+					"type": "crt.ComboboxSearchTextAction",
+					"icon": "combobox-add-new",
+					"caption": "#ResourceString(addRecord_kvju3si_caption)#",
+					"clicked": {
+						"request": "crt.CreateRecordFromLookupRequest",
+						"params": {}
+					}
+				},
+				"parentName": "Owner",
+				"propertyName": "listActions",
+				"index": 0
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfig: /**SCHEMA_VIEW_MODEL_CONFIG*/{
+			"attributes": {
+				"LookupAttribute_uvmgqg9": {
+					"modelConfig": {
+						"path": "OpportunityDS.Type"
+					}
+				},
+				"LookupAttribute_5lqbnzx": {
+					"modelConfig": {
+						"path": "OpportunityDS.Group"
+					}
+				},
+				"LookupAttribute_0gzvy5s": {
+					"modelConfig": {
+						"path": "OpportunityDS.Owner"
+					}
+				}
+			}
+		}/**SCHEMA_VIEW_MODEL_CONFIG*/,
+		modelConfig: /**SCHEMA_MODEL_CONFIG*/{
+			"dataSources": {
+				"OpportunityDS": {
+					"type": "crt.EntityDataSource",
+					"scope": "page",
+					"config": {
+						"entitySchemaName": "Opportunity"
+					}
+				}
+			},
+			"primaryDataSourceName": "OpportunityDS"
+		}/**SCHEMA_MODEL_CONFIG*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
---
+++ CrtLeadOppMgmtApp/Autogenerated/Src/LeadConverting_MiniPage.CrtLeadOppMgmtApp.js
@@ -0,0 +1,295 @@
+﻿define("LeadConverting_MiniPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"name": "PageTitle",
+				"values": {
+					"visible": false
+				}
+			},
+			{
+				"operation": "insert",
+				"name": "Title",
+				"values": {
+					"type": "crt.Label",
+					"caption": "#MacrosTemplateString(#ResourceString(Title_caption)#)#",
+					"labelType": "headline-2",
+					"labelThickness": "default",
+					"labelEllipsis": false,
+					"labelColor": "#0D2E4E",
+					"labelBackgroundColor": "transparent",
+					"labelTextAlign": "start",
+					"visible": true
+				},
+				"parentName": "TitleContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "LeadType",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 1,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.LookupAttribute_1oarpkk",
+					"labelPosition": "above",
+					"control": "$LookupAttribute_1oarpkk",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": [],
+					"visible": true,
+					"placeholder": "",
+					"tooltip": ""
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Budget",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 2,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.NumberInput",
+					"label": "#ResourceString(Budget_label)#",
+					"labelPosition": "above",
+					"control": "$NumberAttribute_z894l1r",
+					"visible": true,
+					"placeholder": "",
+					"tooltip": "#ResourceString(Budget_tooltip)#"
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "DecisionDate",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 3,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.DateTimePicker",
+					"pickerType": "date",
+					"label": "#ResourceString(DecisionDate_label)#",
+					"labelPosition": "above",
+					"control": "$DateTimeAttribute_2eks0zn",
+					"visible": true,
+					"placeholder": "#ResourceString(DecisionDate_placeholder)#",
+					"tooltip": ""
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "ProductSuggestions",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 4,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"multiline": true,
+					"label": "$Resources.Strings.StringAttribute_f1zvxc4",
+					"labelPosition": "above",
+					"control": "$StringAttribute_f1zvxc4",
+					"visible": true,
+					"placeholder": "#ResourceString(ProductSuggestions_placeholder)#",
+					"tooltip": ""
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 3
+			},
+			{
+				"operation": "insert",
+				"name": "SalesChannel",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 5,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.LookupAttribute_453uzzw",
+					"labelPosition": "above",
+					"control": "$LookupAttribute_453uzzw",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": [],
+					"visible": true,
+					"placeholder": "",
+					"tooltip": ""
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 4
+			},
+			{
+				"operation": "insert",
+				"name": "Group",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 6,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.LookupAttribute_z6rv4nw",
+					"labelPosition": "above",
+					"control": "$LookupAttribute_z6rv4nw",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": [],
+					"visible": true,
+					"placeholder": "",
+					"tooltip": ""
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 5
+			},
+			{
+				"operation": "insert",
+				"name": "Owner",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 7,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.ComboBox",
+					"label": "$Resources.Strings.LookupAttribute_aejsoj5",
+					"labelPosition": "above",
+					"control": "$LookupAttribute_aejsoj5",
+					"listActions": [],
+					"showValueAsLink": true,
+					"controlActions": []
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 6
+			},
+			{
+				"operation": "insert",
+				"name": "addRecord_hhgvdo1",
+				"values": {
+					"code": "addRecord",
+					"type": "crt.ComboboxSearchTextAction",
+					"icon": "combobox-add-new",
+					"caption": "#ResourceString(addRecord_hhgvdo1_caption)#",
+					"clicked": {
+						"request": "crt.CreateRecordFromLookupRequest",
+						"params": {}
+					}
+				},
+				"parentName": "Owner",
+				"propertyName": "listActions",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Commentary",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 8,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"multiline": true,
+					"label": "#ResourceString(Commentary_label)#",
+					"labelPosition": "above",
+					"control": "$StringAttribute_92jeqve",
+					"visible": true,
+					"placeholder": "",
+					"tooltip": ""
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 7
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfig: /**SCHEMA_VIEW_MODEL_CONFIG*/{
+			"attributes": {
+				"LookupAttribute_1oarpkk": {
+					"modelConfig": {
+						"path": "LeadDS.LeadType"
+					}
+				},
+				"NumberAttribute_z894l1r": {
+					"modelConfig": {
+						"path": "LeadDS.Budget"
+					}
+				},
+				"DateTimeAttribute_2eks0zn": {
+					"modelConfig": {
+						"path": "LeadDS.DecisionDate"
+					}
+				},
+				"StringAttribute_f1zvxc4": {
+					"modelConfig": {
+						"path": "LeadDS.ProductSuggestions"
+					}
+				},
+				"LookupAttribute_453uzzw": {
+					"modelConfig": {
+						"path": "LeadDS.SalesChannel"
+					}
+				},
+				"LookupAttribute_z6rv4nw": {
+					"modelConfig": {
+						"path": "LeadDS.Group"
+					}
+				},
+				"LookupAttribute_aejsoj5": {
+					"modelConfig": {
+						"path": "LeadDS.Owner"
+					}
+				},
+				"StringAttribute_92jeqve": {
+					"modelConfig": {
+						"path": "LeadDS.Commentary"
+					}
+				}
+			}
+		}/**SCHEMA_VIEW_MODEL_CONFIG*/,
+		modelConfig: /**SCHEMA_MODEL_CONFIG*/{
+			"dataSources": {
+				"LeadDS": {
+					"type": "crt.EntityDataSource",
+					"scope": "page",
+					"config": {
+						"entitySchemaName": "Lead"
+					}
+				}
+			},
+			"primaryDataSourceName": "LeadDS"
+		}/**SCHEMA_MODEL_CONFIG*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
